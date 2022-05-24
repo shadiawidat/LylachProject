ca64acdcb05f81c4a22453ab4ebebcf67fe5f770
shadi <shadiawidat2001@gmail.com>
2022-05-25 00:18:22 +0300

    update shadi

--- Lilach.Project/client/src/main/java/il/cshaifasweng/OCSFMediatorExample/client/Controllers/Account.java
+++ Lilach.Project/client/src/main/java/il/cshaifasweng/OCSFMediatorExample/client/Controllers/Account.java
@@ -1,20 +1,76 @@
 package il.cshaifasweng.OCSFMediatorExample.client.Controllers;
 
-import il.cshaifasweng.OCSFMediatorExample.entities.Client;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 
 public class Account {
 
+    @FXML
+    private TextField AccountType;
 
-    public static Client client;
+    @FXML
+    private TextField Address;
 
+    @FXML
+    private Button BackBtn;
 
+    @FXML
+    private TextField BirthDate;
 
-    public static Client getClient() {
-        return client;
+    @FXML
+    private ImageView CartButton;
+
+    @FXML
+    private TextField CreditCard;
+
+    @FXML
+    private TextField Email;
+
+    @FXML
+    private TextField FirstName;
+
+    @FXML
+    private TextField ID;
+
+    @FXML
+    private TextField LastName;
+
+    @FXML
+    private ImageView MenuBtn;
+
+    @FXML
+    private Button MyOrdersBtn;
+
+    @FXML
+    private TextField Phone;
+
+    @FXML
+    void Back(ActionEvent event) {
+
     }
 
-    public static void setClient(Client cli) {
-        client = cli;
+    @FXML
+    void GoToCart(MouseEvent event) {
+
     }
+
+    @FXML
+    void MenuClick(MouseEvent event) {
+
+    }
+
+    @FXML
+    void SignUp(MouseEvent event) {
+
+    }
+
+    @FXML
+    void ViewOrders(ActionEvent event) {
+
+    }
+
 }
