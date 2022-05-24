6f9a9be54f44fd7c4b910e6b92755fe391811a7b
Nour267 <noorabo7@outlook.com>
2022-05-24 03:42:16 +0300

    workers

---
+++ Lilach.Project/client/src/main/java/il/cshaifasweng/OCSFMediatorExample/client/Controllers/BranchManager.java
@@ -0,0 +1,51 @@
+package il.cshaifasweng.OCSFMediatorExample.client.Controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+
+public class BranchManager {
+
+    @FXML
+    private TextField AccountTybe;
+
+    @FXML
+    private ImageView CartButton;
+
+    @FXML
+    private Button CompareReports;
+
+    @FXML
+    private TextField FirstNameBranchManager;
+
+    @FXML
+    private ImageView MenuBtn;
+
+    @FXML
+    private Button RemoveItem;
+
+    @FXML
+    private Button ShowComplain;
+
+    @FXML
+    private Button UpdateCatalog;
+
+    @FXML
+    private Button ViewReports;
+
+    @FXML
+    private Button addItem;
+
+    @FXML
+    void GoToCart(MouseEvent event) {
+
+    }
+
+    @FXML
+    void MenuClick(MouseEvent event) {
+
+    }
+
+}
