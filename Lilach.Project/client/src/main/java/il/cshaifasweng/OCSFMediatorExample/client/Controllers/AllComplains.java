1abfaa685ed63429bf7eec1fff7dd1bff0514cfe
shadi <shadiawidat2001@gmail.com>
2022-05-28 00:50:38 +0300

    shipping and complains

---
+++ Lilach.Project/client/src/main/java/il/cshaifasweng/OCSFMediatorExample/client/Controllers/AllComplains.java
@@ -0,0 +1,110 @@
+package il.cshaifasweng.OCSFMediatorExample.client.Controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.GridPane;
+
+public class AllComplains {
+
+    @FXML
+    private Button Back;
+
+    @FXML
+    private ImageView CartButton;
+
+    @FXML
+    private MenuItem MenuAbout;
+
+    @FXML
+    private ImageView MenuBtn;
+
+    @FXML
+    private MenuItem MenuCart;
+
+    @FXML
+    private MenuItem MenuProfile;
+
+    @FXML
+    private MenuItem MenuSignIn;
+
+    @FXML
+    private MenuItem MenuSignOut;
+
+    @FXML
+    private MenuItem MenuSignUp;
+
+    @FXML
+    private Label UserName;
+
+    @FXML
+    private GridPane gridPane;
+
+    @FXML
+    private MenuBar menu;
+
+    @FXML
+    private ScrollPane scroll;
+
+    @FXML
+    void Back(MouseEvent event) {
+
+    }
+
+    @FXML
+    void CartClick(MouseEvent event) {
+
+    }
+
+    @FXML
+    void CloseMenu(MouseEvent event) {
+
+    }
+
+    @FXML
+    void GoToAbout(ActionEvent event) {
+
+    }
+
+    @FXML
+    void GoToAccount(MouseEvent event) {
+
+    }
+
+    @FXML
+    void GoToCart(ActionEvent event) {
+
+    }
+
+    @FXML
+    void GoToProfile(ActionEvent event) {
+
+    }
+
+    @FXML
+    void GoToSignIn(ActionEvent event) {
+
+    }
+
+    @FXML
+    void GoToSignOut(ActionEvent event) {
+
+    }
+
+    @FXML
+    void GoToSignUp(ActionEvent event) {
+
+    }
+
+    @FXML
+    void MenuClick(MouseEvent event) {
+
+    }
+
+}
---
+++ Lilach.Project/client/src/main/java/il/cshaifasweng/OCSFMediatorExample/client/Controllers/Shipping.java
@@ -0,0 +1,146 @@
+package il.cshaifasweng.OCSFMediatorExample.client.Controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+
+public class Shipping {
+
+    @FXML
+    private TextField Address;
+
+    @FXML
+    private Button Approve;
+
+    @FXML
+    private Button Back;
+
+    @FXML
+    private TextField Blessing;
+
+    @FXML
+    private ImageView CartButton;
+
+    @FXML
+    private DatePicker Date;
+
+    @FXML
+    private MenuItem MenuAbout;
+
+    @FXML
+    private ImageView MenuBtn;
+
+    @FXML
+    private MenuItem MenuCart;
+
+    @FXML
+    private MenuItem MenuProfile;
+
+    @FXML
+    private MenuItem MenuSignIn;
+
+    @FXML
+    private MenuItem MenuSignOut;
+
+    @FXML
+    private MenuItem MenuSignUp;
+
+    @FXML
+    private TextField Name;
+
+    @FXML
+    private TextField PhoneNumber;
+
+    @FXML
+    private Label Saved;
+
+    @FXML
+    private Label Tax;
+
+    @FXML
+    private Label Total;
+
+    @FXML
+    private Label UserName;
+
+    @FXML
+    private MenuBar menu;
+
+    @FXML
+    void Approve(MouseEvent event) {
+
+    }
+
+    @FXML
+    void Back(MouseEvent event) {
+
+    }
+
+    @FXML
+    void CartClick(MouseEvent event) {
+
+    }
+
+    @FXML
+    void CloseMenu(MouseEvent event) {
+
+    }
+
+    @FXML
+    void Delivery(MouseEvent event) {
+
+    }
+
+    @FXML
+    void ForSomeone(MouseEvent event) {
+
+    }
+
+    @FXML
+    void GoToAbout(ActionEvent event) {
+
+    }
+
+    @FXML
+    void GoToAccount(MouseEvent event) {
+
+    }
+
+    @FXML
+    void GoToCart(ActionEvent event) {
+
+    }
+
+    @FXML
+    void GoToProfile(ActionEvent event) {
+
+    }
+
+    @FXML
+    void GoToSignIn(ActionEvent event) {
+
+    }
+
+    @FXML
+    void GoToSignOut(ActionEvent event) {
+
+    }
+
+    @FXML
+    void GoToSignUp(ActionEvent event) {
+
+    }
+
+    @FXML
+    void MenuClick(MouseEvent event) {
+
+    }
+
+}
